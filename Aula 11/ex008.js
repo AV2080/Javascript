cc075a41a570cd96b0022e967f95f7ef3d1f1539
Alexandre Vitale Alves <123398521+AV2080@users.noreply.github.com>
2023-02-25 15:05:21 -0300

    Atualizei javascript

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60){ // condição simples!
+    console.log('Você ultrapassou o limite de velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!') 
